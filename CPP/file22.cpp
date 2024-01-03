ea83ff84511e3dea60fa798cfe245b1c77200261
Coding_Ultron <99073430+gyanshivam@users.noreply.github.com>
2024-01-03 07:54:22 +0530

    Create file22.cpp

---
+++ CPP/file22.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int x;
+    long int y;
+    char z;
+    float f;
+    double d;
+    
+    cin>>x>>y>>z>>f>>d;
+    cout<<x<<endl<<y<<endl<<z<<endl<<fixed<<setprecision(3)<<f<<endl<<fixed<<setprecision(9)<<d;
+return 0;
+}
