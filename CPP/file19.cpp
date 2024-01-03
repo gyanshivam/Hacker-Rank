0bfc797b436da79ccb9b85a1f3bb09f2d2bcf40e
Coding_Ultron <99073430+gyanshivam@users.noreply.github.com>
2024-01-03 07:50:35 +0530

    Update file19.cpp

--- CPP/file19.cpp
+++ CPP/file19.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    string num[] ={"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}; 
+    
+    cin>>a>>b;
+    
+    for(int i=a-1; a<=b; a++,i++)
+    {
+        if(a<=9)
+        {
+            cout<<num[i]<<endl;
+        }
+        else
+        {
+           if(a%2==0) 
+                cout<<"even\n";
+            else
+                cout<<"odd\n";
+        }
+    }
+    return 0;
+}
 
