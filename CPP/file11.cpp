12480575864dcb9bc83450a01531a9f1beeab679
Coding_Ultron <99073430+gyanshivam@users.noreply.github.com>
2024-01-03 07:41:53 +0530

    Create file11.cpp

---
+++ CPP/file11.cpp
@@ -0,0 +1,21 @@
+
+
+    
+    // Declare second integer, double, and String variables.
+    int i2;
+    double d2;
+    string s2;
+    
+    // Read and save an integer, double, and String to your variables.
+    
+    cin>>i2>>d2;
+    getline(cin>>ws,s2);
+     
+    // Print the sum of both integer variables on a new line.
+    cout<<i+i2;
+   // Print the sum of the double variables on a new line.
+    cout<<endl<<fixed<<setprecision(1)<<d+d2; 
+    // Concatenate and print the String variables on a new line
+    cout<<endl<<s+s2;
+    // The 's' variable above should be printed first.
+
